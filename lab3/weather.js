40f86b0dff4b84633134b272b87ddb07385216d0
Niko Prassas <prassasniko@gmail.com>
2021-08-22 21:40:33 -0400

    implement most functionality for lab3

--- lab3/weather.js
+++ lab3/weather.js
@@ -1,17 +1,21 @@
-var appid = "babde2779459e0c9acc24f2ec0442ba1";  // insert valid APPID here
+var APPID = "babde2779459e0c9acc24f2ec0442ba1";
+
+function showError(errorMsg) {
+  var resp = document.getElementById("response");
+  resp.style["display"] = "";
+  var el = document.getElementById("error");
+  document.getElementById("results").style["display"] = "none";
+  el.innerHTML = errorMsg;
+  el.style["display"] = "";
+}
 
 function onWeather(err, data) {
   if (err) {
-    /* TODO:
-     * Toggle the results display for the response display.
-     * Print an error message in the error element and change its display so
-     * that it is no longer hidden.
-     */
-    var el = document.getElementById("error");
+    showError("Could not find any weather data for the given zip code");
     return;
   }
 
-  // TODO: Empty the error element and hide it.
+  document.getElementById("error").style["display"] = "none";
 
   var el = document.getElementById('response');
 
@@ -19,57 +23,49 @@ function onWeather(err, data) {
   document.getElementById("temp").innerHTML = temp + " &deg;F";
 
   var windspeed = data.wind.speed;
-  // TODO: Set the element with ID windspeed's content to the windspeed above,
-  // with the unit "mph" afterwards
+  document.getElementById("windspeed").innerHTML = `${windspeed} mph`;
 
   var iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
   var iconEl = document.getElementById("icon");
-  // TODO: Create an img tag with src set to iconUrl, and set the content of the
-  // icon element to that image.
+
+  var iconImg = document.createElement("img");
+  iconImg.setAttribute("src", iconUrl);
+
+  if (iconEl.firstChild) {
+    iconEl.removeChild(icon.firstChild);
+  }
+  iconEl.appendChild(iconImg);
 
   var locationEl = document.getElementById("location");
   locationEl.innerHTML = data.name;
 
-  // TODO: Make the response element and results elements both visible
+  el.style["display"] = "";
+  document.getElementById("results").style["display"] = "";
 }
 
 function onZipCode(err, data) {
   if (err) {
-    /* TODO:
-     * Toggle the results display for the response display.
-     * Print an error message in the error element and change its display so
-     * that it is no longer hidden.
-     */
-    var el = document.getElementById("error");
-    return;
+      showError("That zip code does not exist");
+      return;
   }
   var firstMatch = data.places[0];
-  /* TODO:
-   * Get the city name, state name and country from the place data returned by
-   * the Zippopotamus API.
-   */
+  var city = firstMatch["place name"];
+  var state = firstMatch.state;
+  var country = data.country;
+  
   var url = "http://api.openweathermap.org/data/2.5/weather";
-  /* TODO:
-   * Access the url above with the query string below:
-   *   ?APPID=[APPID]&units=imperial&q=[CITY],[STATE],[COUNTRY]
-   * Where the things in brackets were found above.
-   */
+  var url_with_params = url + `?APPID=${APPID}&units=imperial&q=${city},${state},${country}`;
+  AJAX.getJSON(url_with_params, onWeather);
 }
 
 function getWeather(e) {
   e.preventDefault(); // stop submit
   var zipCode = document.getElementById("zipCode").value;
   if (!zipCode) {
-    /* TODO:
-     * Toggle the results display for the response display.
-     * Print an error message in the error element and change its display so
-     * that it is no longer hidden.
-     */
-    var el = document.getElementById("error");
+    showError("Must enter a zip code");
     return;
   }
-  /* TODO:
-   * Access the url http://api.zippopotam.us/us/ZZZZZ where ZZZZ is the given
-   * zip code.
-   */
+
+  var url = `http://api.zippopotam.us/us/${zipCode}`;
+  AJAX.getJSON(url, onZipCode);
 }
